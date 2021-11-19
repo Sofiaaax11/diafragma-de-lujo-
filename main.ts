4771a1cb8b99cab2bf50c1f631cf87f043a11d2d
Sofiaaax11 <94657403+Sofiaaax11@users.noreply.github.com>
2021-11-18 19:58:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+for (let index = 0; index < 3; index++) {
+    basic.showString("\"aplaudir\"")
+}
+for (let index = 0; index < 2; index++) {
+    basic.showString("\"manos a la cintura \"")
+    basic.showString("\"aplaudir\"")
+    basic.showString("\"manos la cabeza\"")
+}
+basic.showString("\"mover los hombros\"")
+basic.showString("\"otro compañero esta asciendo la coreografía ")
